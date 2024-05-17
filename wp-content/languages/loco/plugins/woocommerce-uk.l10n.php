f68c0ff2e81a54d9ce8d0672897f0c62a4056864
Sergiy Tomko <thomas@MacBook-Air-Sergiy.local>
2024-05-17 15:45:52 +0300

    optimized home page for all devices

---
+++ wp-content/languages/loco/plugins/woocommerce-uk.l10n.php
@@ -0,0 +1,3 @@
+<?php
+return ['project-id-version'=>'WooCommerce 8.8.3','report-msgid-bugs-to'=>'https://wordpress.org/support/plugin/woocommerce','last-translator'=>'','language-team'=>'Українська','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','pot-creation-date'=>'2024-04-27T13:22:02+00:00','po-revision-date'=>'2024-05-16 21:13+0000','x-generator'=>'Loco https://localise.biz/','x-domain'=>'woocommerce','language'=>'uk','plural-forms'=>'nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10 >= 2 && n%10<=4 &&(n%100<10||n%100 >= 20)? 1 : 2);','x-loco-version'=>'2.6.9; wp-6.5.3','messages'=>['block titleCheckout'=>'оплата
+']];
